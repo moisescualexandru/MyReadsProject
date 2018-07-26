c3e49d7015b29265bf03812ed6d5fe378c19d36b
Alexandru Moisescu <moisescu.alexandru88@gmail.com>
2018-07-26 22:02:41 +0300

    Added the required methods and state for the main app (getAll books, updateShelf and addBook)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,38 @@ import { Link } from 'react-router-dom';
 import Search from './Search';
 
 class App extends Component {
+
+  state = {
+    books: [],
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books });
+    }).catch(error => console.log(error));
+  }
+
+  updateShelf = (bookId, shelf) => {
+    BooksAPI.update({id: bookId}, shelf).then((book) => {
+      BooksAPI.getAll().then((books) => {
+        this.setState({ books });
+    })
+      
+    }).catch(error => console.log(error));
+    
+  }
+
+  addBook = (bookId, shelf) => {
+    BooksAPI.get(bookId).then((book) => {
+      this.setState((state) => ({
+        books: state.books.concat([ book ])
+      }))
+    }).then(() => {
+      this.updateShelf(bookId, shelf);
+    }).catch(error => console.log(error));
+    
+  }
+
   render() {
     return (
       <div className='app'>
@@ -15,7 +47,8 @@ class App extends Component {
           <div className='list-books'>
             <MyBooks />
             <Bookshelf 
-
+            books={this.state.books}
+            updateShelf={this.updateShelf}
             />
             <div className='open-search'>
               <Link to='/search'/>
@@ -26,10 +59,15 @@ class App extends Component {
 
         <Route path='/search' render={({ history }) => (
           <Search 
-          
+           addBook={(bookId, shelf) => {
+            this.addBook(bookId, shelf);
+            history.push('/');
+            }}
+           books={this.state.books}
           />
         )} />
       </div>
+    )
   }
 }
 
