44ed0726f946eecedb34d904ef89608b784a3747
Alexandru Moisescu <moisescu.alexandru88@gmail.com>
2018-07-26 22:08:23 +0300

    Created the addShelf method in order to display the right shelf for the searched books

--- src/Search.js
+++ src/Search.js
@@ -13,7 +13,30 @@ class Search extends Component {
 		this.setState({ query: query });
 		BooksAPI.search(query).then((books) => {
 			this.setState({ searched: books });
-		})
+		}).then(() => this.state.query && this.state.searched.error !== 'empty query' ? this.addShelf() : '')
+	}
+
+	addShelf() {
+		this.setState(state => ({
+			searched: state.searched.filter(book => book.shelf = 'none') 
+		}));
+
+		const array = this.props.books;
+
+		let booksOnShelf = this.state.searched.filter(function(book) {
+			return array.filter(function(bookOnShelf) {
+				return bookOnShelf.id === book.id ? book.shelf = bookOnShelf.shelf : '';
+			}).length !== 0
+		});
+
+		let booksNotOnShelf = this.state.searched.filter(function(book) {
+			return array.filter(function(bookNotOnShelf) {
+				return bookNotOnShelf.id === book.id;
+			}).length === 0
+		});
+
+		this.setState({ searched: booksNotOnShelf.concat(booksOnShelf) })
+
 	}
 
 	render() {
@@ -30,9 +53,12 @@ class Search extends Component {
 						/>
 					</div>
 				</div>
+				{this.state.searched && this.state.searched.error !== 'empty query' && this.state.query && (
 					<SearchList 
-					
+						searched={this.state.searched}
+						addBook={this.props.addBook}
 					/>
+				)}				
 			</div>
 		)
 	}
