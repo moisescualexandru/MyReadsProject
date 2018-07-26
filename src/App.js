bc857532c9526cdbcde48c2af843de55817c6eb3
Alexandru Moisescu <moisescu.alexandru88@gmail.com>
2018-07-26 21:54:09 +0300

    Added components MyBooks, Bookshelf and Search to the main app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom'; 
 import './App.css';
+import MyBooks from './MyBooks'
+import Bookshelf from './Bookshelf';
+import * as BooksAPI from './BooksAPI';
+import { Link } from 'react-router-dom';
+import Search from './Search';
 
 class App extends Component {
   render() {
     return (
+      <div className='app'>
+        <Route exact path='/' render={() => (
+          <div className='list-books'>
+            <MyBooks />
+            <Bookshelf 
 
+            />
+            <div className='open-search'>
+              <Link to='/search'/>
+            </div>
+          </div>
+        )}
+        />
+
+        <Route path='/search' render={({ history }) => (
+          <Search 
+          
+          />
+        )} />
+      </div>
   }
 }
 
